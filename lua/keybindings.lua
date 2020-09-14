408b02c8cd87397c03c5fafad9decaa03248db47
Jerome Reinlaender <jerome.reinlaneder@gmail.com>
2020-09-14 23:44:43 +0200

    Add navigation keybindings if lines are broken

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -6,9 +6,6 @@ vim.api.nvim_set_keymap('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', { norema
 vim.api.nvim_set_keymap('n', 'gD', ':lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'gr', ':lua vim.lsp.buf.references()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'g0', ':lua vim.lsp.buf.document_symbol()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'gW', ':lua vim.lsp.buf.workspace_symbol()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'gw', ':lua vim.lsp.buf.workspace_symbol()<CR>', { noremap = true, silent = true })
 
 
 -- more convenient omnifunc
@@ -16,7 +13,16 @@ vim.api.nvim_set_keymap('i', '<C-A>', '<C-X><C-O>', { noremap = false, silent = 
 
 
 -- NAVIGATION
+
+-- window
 vim.api.nvim_set_keymap('n', '<C-H>', '<C-W>h', { noremap = false, silent = true })
 vim.api.nvim_set_keymap('n', '<C-J>', '<C-W>j', { noremap = false, silent = true })
 vim.api.nvim_set_keymap('n', '<C-K>', '<C-W>k', { noremap = false, silent = true })
 vim.api.nvim_set_keymap('n', '<C-L>', '<C-W>l', { noremap = false, silent = true })
+-- lines
+vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', '$', 'g$', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', '0', 'g0', { noremap = false, silent = true })
+
+
