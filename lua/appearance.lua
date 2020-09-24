91094f015333dc47ea3c4b6d43be12af73369879
Jerome Reinlaender <jreinlaender@byte5.de>
2020-09-24 10:27:34 +0200

    Open windows on the right side or below

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -14,3 +14,7 @@ vim.o.tabstop = 2
 -- how many spaces (or equivalents) is one indent
 vim.bo.shiftwidth = 2
 vim.o.shiftwidth = 2
+
+-- open windows to the right or below
+vim.o.splitright = true
+vim.o.splitbelow = true
