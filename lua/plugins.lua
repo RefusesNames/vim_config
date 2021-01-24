90193739894a4c8dd2ad4cce6aefbdd296a6a62a
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2021-01-24 15:04:02 +0100

    Use correct branches for plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,13 +7,14 @@ vim.cmd [[packadd packer.nvim]]
 local packer = require('packer')
 packer.startup(function()
 	use { 'wbthomason/packer.nvim', opt=true }
-	use { 'mizux/vim-colorschemes', opt=true }
+	use { 'mizux/vim-colorschemes', opt=true, branch='main' }
 	use { 'norcalli/nvim-colorizer.lua', as='colorizer', config=function() require('colorizer').setup() end }
 	-- use { 'neovim/nvim-lspconfig', config=configureLanguageServer }
 	use { 'neovim/nvim-lspconfig', config=configureLanguageServer }
 	use { 'nvim-lua/completion-nvim' }
 
 	use { 'tpope/vim-commentary' }
+	use { 'tpope/fugitive', branch='main' }
 	
 	-- TODO: maybe replace with lua equivalents:
 	use {
