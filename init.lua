10f83aab833f097f185ea8775fe246793f094197
Jerome Reinlaender <jreinlaender@conet.de>
2021-04-20 08:46:00 +0200

    Remove treesitter completely

--- init.lua
+++ init.lua
@@ -23,15 +23,6 @@ require('plugins')
 require('appearance')
 require('keybindings')
 
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-    disable = { "c", "rust" },  -- list of language that will be disabled
-  },
-	indent = { enable = true },
-}
-
 -- detect the filetype
 vim.api.nvim_set_option('filetype', 'on')
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,15 +9,10 @@ packer.startup(function()
 	use { 'wbthomason/packer.nvim', opt=true }
 	use { 'mizux/vim-colorschemes', opt=true, branch='main' }
 	use { 'norcalli/nvim-colorizer.lua', as='colorizer', config=function() require('colorizer').setup() end }
-	use { 'neovim/nvim-lspconfig', config=function() configureLanguageServer() end }
-	use { 'nvim-lua/completion-nvim' }
 
 	use { 'tpope/vim-commentary' }
 	use { 'tpope/vim-fugitive' }
 
-	-- use { 'nvim-treesitter/nvim-treesitter', config=function() vim.cmd('TSUpdate') end }
-	use { 'nvim-treesitter/nvim-treesitter' }
-	
 	-- TODO: maybe replace with lua equivalents:
 	use {
 		'junegunn/fzf.vim',
@@ -37,10 +32,5 @@ packer.startup(function()
 	use { 'thiagoalessio/rainbow_levels.vim', as='rainbow_levels' }
 
 end)
-
-
-function configureLanguageServer()
-	local lsp = require('lspconfig')
-end
 
 return packer
