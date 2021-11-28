18bc1100412a04b804b27d61b7332810d15d5ca1
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2021-11-28 22:20:06 +0100

    First usable multi platform configuration
    - plugin manager
    - color scheme
    - explorer
    - comment
    - keybindings for navigation

---
+++ lua/plugins.lua
@@ -0,0 +1,34 @@
+
+-- packer
+local file_system = vim.fn
+local packer_install_dir = file_system.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local packer_repo = 'https://github.com/wbthomason/packer.nvim'
+local install_cmd = string.format('10split |term git clone --depth=1 %s %s', packer_repo, packer_install_dir)
+
+local packer_bootstrap = false
+
+if file_system.empty(file_system.glob(packer_install_dir)) > 0 then
+	vim.api.nvim_echo({{'Installing packer.nvim', 'Type'}}, true, {})
+	-- vim.api.nvim_command(install_cmd)
+	packer_bootstrap = vim.api.nvim_command(install_cmd)
+	-- packer_bootstrap = file_system.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', packer_install_dir})
+	vim.api.nvim_command('packadd packer.nvim')
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use { 'wbthomason/packer.nvim' }
+	use { 'rmehri01/onenord.nvim' }
+	use { 'kyazdani42/nvim-tree.lua' }
+	use {
+	    'numToStr/Comment.nvim',
+	    config = function()
+		require('Comment').setup()
+	    end
+	}
+
+	if packer_bootstrap then
+		require('packer').sync()
+	end
+end)
