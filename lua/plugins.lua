93d000a8bf3d711f000f17b3b5e95c902bc03ccf
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2022-01-28 22:45:38 +0100

    Add lualine

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -35,6 +35,14 @@ return require('packer').startup(function(use)
 		}
 	}
 
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+		config = function()
+			require('lualine').setup{}
+		end
+	}
+
 	use { 'tpope/vim-fugitive' }
 
 	use {
