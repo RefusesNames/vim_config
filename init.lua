ceac25996b8a94aad9c23f43b36fb22369ce318f
Jerome Reinlaender <jreinlaender@exocad.com>
2023-10-16 09:39:51 +0200

    Add user command for taking notes

--- init.lua
+++ init.lua
@@ -43,4 +43,4 @@ require('resharper')
 -- vim.api.nvim_command('autocmd Filetype cs setlocal omnifunc=v:lua.vim.lsp.omnifunc')
 -- vim.api.nvim_command('autocmd TermOpen * setlocal nonumber norelativenumber')
 
--- vim.api.nvim_create_user_command('Notes', 'e ~/notes.md', { force = true })
+vim.api.nvim_create_user_command('Notes', 'e ~/notes.md', { force = true })
