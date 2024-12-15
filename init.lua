c51ec147f72a56368cdaf88681e8136bce49cc06
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2024-12-15 22:46:50 +0100

    Add command to reload config (:ReloadConfig)

--- init.lua
+++ init.lua
@@ -60,3 +60,27 @@ vim.api.nvim_create_user_command('Notes', 'e ~/notes.md', { force = true })
 vim.api.nvim_create_user_command('Vgit', function() require('neogit').open({ kind = 'vsplit' }) end, { force = true })
 vim.api.nvim_create_user_command('Sgit', function() require('neogit').open({ kind = 'split' }) end, { force = true })
 vim.api.nvim_create_user_command('Tgit', function ()require('neogit').open({ kind = 'tab' }) end, { force = true })
+
+
+-- Function to reload NeoVim configuration and ftplugin settings
+function ReloadConfig()
+	-- Unload all Lua modules
+	for name,_ in pairs(package.loaded) do
+		if name:match('^user') or name:match('^plugins') then
+			package.loaded[name] = nil
+		end
+	end
+
+	-- Source the init.lua file
+	dofile(vim.env.MYVIMRC)
+
+	-- Reload ftplugin settings for all open buffers
+	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.api.nvim_buf_is_loaded(bufnr) then
+			local ft = vim.api.nvim_buf_get_option(bufnr, 'filetype')
+			vim.cmd('doautocmd FileType ' .. ft)
+		end
+	end
+end
+-- Create a command to reload the configuration
+vim.api.nvim_create_user_command('ReloadConfig', 'lua ReloadConfig()', {})
