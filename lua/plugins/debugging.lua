a41ab5cdeb09a9f808a306645b20ad42c87a32fb
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2025-02-20 18:18:42 +0100

    Add documentation
    - documentation for debugging plugins
    - netcoredbg path for local settings example

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,10 +1,69 @@
 return {
+	-- source: https://github.com/mfussenegger/nvim-dap
 	'mfussenegger/nvim-dap',
+	-- USAGE:
+	--
+	--   Setting breakpoints via :lua require'dap'.toggle_breakpoint().
+	--   Launching debug sessions and resuming execution via :lua require'dap'.continue().
+	--   Stepping through code via :lua require'dap'.step_over() and :lua require'dap'.step_into().
+	--   Inspecting the state via the built-in REPL: :lua require'dap'.repl.open() or using the widget UI (:help dap-widgets)
+	-- REQUIREMENTS:
+	--   netcoredbg
 	{
+		-- source: https://github.com/rcarriga/nvim-dap-ui
 		'rcarriga/nvim-dap-ui',
 		dependencies = {
 			'mfussenegger/nvim-dap',
 			'nvim-neotest/nvim-nio'
-		}
+		},
+		config = function()
+			-- this automatically starts the DAP-UI when DAP is started
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			-- USAGE:
+			--
+			-- Keybindings
+			--   edit: e
+			--   expand: <CR>
+			--   open: o
+			--   remove: d
+			--   repl: r
+			--   toggle: t
+			--
+			-- Evaluation
+			--   evaluate expression: require("dapui").eval(<expression>)
+			--   evaluate stuff under the cursor: require("dapui").eval()
+			--   bind it to a key: vnoremap <M-k> <Cmd>lua require("dapui").eval()<CR>
+			--
+			-- Open Floating Element
+			--   require("dapui").float_element(<element ID>, <optional settings>)
+			--
+			--   If no floating element is provided, it queries for one.
+			--   Optional settings are:
+			--     width: number Width of the window
+			--     height: number Height of the window
+			--     enter: boolean Enter the floating window
+			--     position: string Position of floating window. center or nil
+			--   Element IDs are:
+			--     scopes: shows available scopes with variables in them
+			--     stacks: displays running threads and stack frames
+			--     watches: allows creation of expressions to watch
+			--     breakpoints: list breakpoints
+			--     repl: just a REPL
+			--     console: console window used for the integrated terminal
+			--
+		end
 	}
 }
