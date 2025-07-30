2e6c3b8663507d13dd2e7d72d1b53eeacbee3556
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2025-07-30 22:10:25 +0200

    Add keybinding to use telescope on buffer, add descriptions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,21 +5,28 @@ return
 		lazy = true,
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		keys = {
-			{ '<leader><space>', require('telescope.builtin').find_files },
+			{ '<leader><space>', require('telescope.builtin').find_files,
+				desc = 'Fuzzy search a file' },
 			{ '<leader>c', function()
 				require('telescope.builtin').find_files{ cwd = vim.fn.stdpath('config') }
-			end },
-			{ '<leader>o', require("telescope.builtin").treesitter },
-			{ '<leader>f', require("telescope.builtin").live_grep },
+			end,
+				desc = 'Fuzzy search in Neovim configuration' },
+			{ '<leader>o', require('telescope.builtin').treesitter,
+				desc = 'Fuzzy search in tree' },
+			{ '<leader>ff', require('telescope.builtin').live_grep,
+				desc = 'Fuzzy search in all files' },
+			{ '<leader>fb', require('telescope.builtin').current_buffer_fuzzy_find,
+				desc = 'Fuzzy search in current buffer' },
 			{ '<leader>m', function()
-				require("telescope.builtin").treesitter{ symbols="method" }
-			end },
+					require('telescope.builtin').treesitter{ symbols='method' }
+				end,
+				desc = 'Fuzzy search in methods' },
 		},
 		config = function()
 			local telescope = require('telescope')
 			telescope.setup {
 				defaults = {
-					file_ignore_patterns = { "^.git", "^.svn", "^bin", "^obj", "^target" }
+					file_ignore_patterns = { '^.git', '^.svn', '^bin', '^obj', '^target' }
 				}
 			}
 		end
