2e38c4beef80fcb468efbd124e946b4ac5a11004
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2025-10-25 22:29:49 +0200

    Temporarily (?) remove keybindings for hjkl

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -5,10 +5,10 @@ vim.api.nvim_set_keymap('n', '<C-J>', '<C-W>j', { noremap = false, silent = true
 vim.api.nvim_set_keymap('n', '<C-K>', '<C-W>k', { noremap = false, silent = true })
 vim.api.nvim_set_keymap('n', '<C-L>', '<C-W>l', { noremap = false, silent = true })
 -- lines
-vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = false, silent = true })
-vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = false, silent = true })
-vim.api.nvim_set_keymap('n', '$', 'g$', { noremap = false, silent = true })
-vim.api.nvim_set_keymap('n', '0', 'g0', { noremap = false, silent = true })
+-- vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = false, silent = true })
+-- vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = false, silent = true })
+-- vim.api.nvim_set_keymap('n', '$', 'g$', { noremap = false, silent = true })
+-- vim.api.nvim_set_keymap('n', '0', 'g0', { noremap = false, silent = true })
 
 -- Miscellaneous
 vim.api.nvim_set_keymap('n', '<leader>n', ':nohlsearch<CR>', { noremap = false, silent = true})
