8fd1d73c924dde780c72254c45d4528de4af1526
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2025-11-04 18:42:03 +0100

    Fix keybinding for :nohlsearch
    It was bound by lazy to lspconfig

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,15 +1,15 @@
 -- NAVIGATION
 -- window
-vim.api.nvim_set_keymap('n', '<C-H>', '<C-W>h', { noremap = false, silent = true })
-vim.api.nvim_set_keymap('n', '<C-J>', '<C-W>j', { noremap = false, silent = true })
-vim.api.nvim_set_keymap('n', '<C-K>', '<C-W>k', { noremap = false, silent = true })
-vim.api.nvim_set_keymap('n', '<C-L>', '<C-W>l', { noremap = false, silent = true })
+vim.keymap.set('n', '<C-H>', '<C-W>h', { noremap = false, silent = true })
+vim.keymap.set('n', '<C-J>', '<C-W>j', { noremap = false, silent = true })
+vim.keymap.set('n', '<C-K>', '<C-W>k', { noremap = false, silent = true })
+vim.keymap.set('n', '<C-L>', '<C-W>l', { noremap = false, silent = true })
 -- lines
--- vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = false, silent = true })
--- vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = false, silent = true })
--- vim.api.nvim_set_keymap('n', '$', 'g$', { noremap = false, silent = true })
--- vim.api.nvim_set_keymap('n', '0', 'g0', { noremap = false, silent = true })
+-- vim.keymap.set('n', 'j', 'gj', { noremap = false, silent = true })
+-- vim.keymap.set('n', 'k', 'gk', { noremap = false, silent = true })
+-- vim.keymap.set('n', '$', 'g$', { noremap = false, silent = true })
+-- vim.keymap.set('n', '0', 'g0', { noremap = false, silent = true })
 
 -- Miscellaneous
-vim.api.nvim_set_keymap('n', '<leader>n', ':nohlsearch<CR>', { noremap = false, silent = true})
+vim.keymap.set('n', '<leader>j', ':nohlsearch<CR>', { silent = true})
 
