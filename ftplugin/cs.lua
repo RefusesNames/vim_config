9ef4dfa5299df5997bb33db5474c3a70c174b42d
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2025-12-30 11:10:22 +0100

    WIP: Use new version of treesitter (breaking change)

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -4,3 +4,5 @@ vim.opt.makeprg = 'dotnet build --no-incremental /clp:nosummary'
 -- example: D:\Projekte\csharp\timetracking\CliFrontend\Program.cs(22,1): warning CS0164: This label has not been referenced [D:\Projekte\csharp\timetracking\CliFrontend\CliFrontend.csproj]
 -- Set errorformat to match the output of 'dotnet build'
 vim.opt.errorformat = '%f(%l\\,%c):\\ %t%*[^:]:%m,%-G%.%#'
+
+vim.treesitter.start()
