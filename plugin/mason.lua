7ad4cc94a900d3465dae225b3e10d2fb4282e034
Jerome Reinlaender <jerome.reinlaender@gmail.com>
2026-04-12 14:05:53 +0200

    Add some lazy loading

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,30 +1,35 @@
-vim.pack.add({
-	'https://github.com/mason-org/mason.nvim',
-	'https://github.com/mason-org/mason-lspconfig.nvim',
-})
+vim.api.nvim_create_user_command(
+	'LoadMason',
+	function()
+		vim.pack.add({
+			'https://github.com/mason-org/mason.nvim',
+			'https://github.com/mason-org/mason-lspconfig.nvim',
+		})
 
-require("mason").setup({
-	registries = {
-		"github:mason-org/mason-registry",
-		"github:Crashdummyy/mason-registry",
-	}
-})
+		require("mason").setup({
+			registries = {
+				"github:mason-org/mason-registry",
+				"github:Crashdummyy/mason-registry",
+			}
+		})
 
-local mason_registry = require("mason-registry")
+		local mason_registry = require("mason-registry")
 
--- Ensure Roslyn is installed
-if not mason_registry.is_installed("roslyn") then
-	mason_registry.get_package("roslyn"):install()
-end
+		-- Ensure Roslyn is installed
+		if not mason_registry.is_installed("roslyn") then
+			mason_registry.get_package("roslyn"):install()
+		end
 
 
--- Ensure netcoredbg is installed
-if not mason_registry.is_installed("netcoredbg") then
-	mason_registry.get_package("netcoredbg"):install()
-end
+		-- Ensure netcoredbg is installed
+		if not mason_registry.is_installed("netcoredbg") then
+			mason_registry.get_package("netcoredbg"):install()
+		end
 
-local mason_lspconfig = require('mason-lspconfig')
+		local mason_lspconfig = require('mason-lspconfig')
 
-mason_lspconfig.setup({
-	ensure_installed = { "lua_ls", "ts_ls", "html", "cssls", "powershell_es" },
-})
+		mason_lspconfig.setup({
+			ensure_installed = { "lua_ls", "ts_ls", "html", "cssls", "powershell_es" },
+		})
+	end, { force = true }
+)
